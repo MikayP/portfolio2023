53b6b35a9ecc00f0ad6cf700be32f4ffc2f9d5f2
Mike Portman <mikeportmanj@gmail.com>
2023-04-08 20:11:14 -0500

    google analyics

--- functions.php
+++ functions.php
@@ -43,3 +43,18 @@ function post_title_shortcode(){
 add_shortcode('post_title','post_title_shortcode');
 
 
+add_action('wp_head','my_analytics', 20);
+function my_analytics() {
+?>
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-MKEEY78DWZ"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-MKEEY78DWZ');
+</script>
+<?php
+}
